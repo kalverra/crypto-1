0d09346a138afda7378e0174790c5fcb118c190c
Adam Hamrick <adam.hamrick@smartcontract.com>
2023-11-27 09:27:47 -0500

    printing

--- week-2/programming-assignment.py
+++ week-2/programming-assignment.py
@@ -45,11 +45,11 @@ decoded_cbc = decode_cbc("140b41b22a29beb4061bda66b6747e14", "5b68629feb8606f9a6
 print(decoded_cbc)
 print()
 
-print("CTR 3")
+print("CTR 1")
 decoded_ctr = decode_ctr("36f18357be4dbd77f050515c73fcf9f2", "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329")
 print(decoded_ctr)
 print()
 
-print("CTR 4")
+print("CTR 2")
 decoded_ctr = decode_ctr("36f18357be4dbd77f050515c73fcf9f2", "770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451")
 print(decoded_ctr)
